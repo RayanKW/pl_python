77f89d0dfefb5c039f6dd1bff641d7ed883b44a5
RayanKW <wairimurayyan@gmail.com>
2023-01-10 11:37:24 +0300

    python

---
+++ control_statements.py
@@ -0,0 +1,46 @@
+# myNumber=float(input('Please input a number: '))
+# if (myNumber == 7):
+#     print('yey')
+# if(myNumber > 7):
+#     print('hey')
+# if (myNumber < 7):
+#     print("hry")
+
+# #get user input and determine if odd or even the print is odd or is even!
+
+# userNum = int(input('Please input a number: '))
+# if (userNum % 2) == 0:
+#     print('the number is even')
+# else:
+#     print('Number is odd!!')
+# #alternatively
+# num1 = int(input("Please input your Number: "))
+# rem = (num1%2)
+# if (rem == 0):
+#     print("Number is even")
+# if (rem !=0):
+#     print("number is odd")
+#conditionals
+
+# myColor = input("please input your color: ")
+# if (myColor == "Red" or myColor == 'red'):
+#     print("your color is Red")
+# if (myColor !="Red" and myColor != 'red' and myColor !='RED'):
+#     print("color is not red")
+
+#exercise. If a number is between 5 and 10 give
+# feedback it else else give feedback its not!
+
+# num = int(input('please input the number: '))
+# if (num <=10 and num >=5):
+#     print('num is between 5 & 10')
+# else:
+#     print('number is not between 5&10')
+    
+    #for loop
+fruits = ['apple', 'banana', 'orange', 'kiwi']
+for fruit in fruits:
+    print(fruit)
+    
+for myNum in range(1, 20, 5):
+    print (myNum)
